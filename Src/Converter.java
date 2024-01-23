3c1270d310c4c91630e2793965fa60c32c2abea1
Nikasan1 <nikasan4@gmail.com>
2024-01-23 20:48:39 +0100

    Added conversion method (miles in km)

--- Src/Converter.java
+++ Src/Converter.java
@@ -1,2 +1,13 @@
-public class Converter {
+import java.util.Scanner;
+
+public class Converter {public static void main(String[] args) {
+    Converter.conversion();
+}
+    public static void conversion(){
+        System.out.println("Введіть число в милях, для конвертації в км:");
+        Scanner scanner = new Scanner(System.in);
+        double convertNumber = scanner.nextDouble();
+        double convertedNumber = convertNumber*1.6;
+        System.out.printf("%s миль, приблизно: %s км.\n", convertNumber, convertedNumber);
+    }
 }
