15ef49ac2ea200167c118c9345958edd2bbd0968
Nikasan1 <nikasan4@gmail.com>
2024-01-23 20:47:55 +0100

    Added conversion method

--- Src/Converter.java
+++ Src/Converter.java
@@ -1,3 +1,14 @@
-public class Converter {
+import java.util.Scanner;
 
+public class Converter {
+    public static void main(String[] args) {
+        Converter.conversion();
+    }
+    public static void conversion(){
+        System.out.println("Введіть число в км, для конвертації в милі:");
+        Scanner scanner = new Scanner(System.in);
+        double convertNumber = scanner.nextDouble();
+        double convertedNumber = convertNumber*0.62;
+        System.out.printf("%s км в милях, приблизно: %s миль.\n", convertNumber, convertedNumber);
+    }
 }
